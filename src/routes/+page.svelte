1ea8815cf12fbc2c9594a9304f16f4cff096e915
Percy <percy@railway.app>
2023-12-29 05:31:48 +0000

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Twitch Hype</title>
+</svelte:head>
+
+<section class="page" id="home">
+	<section class="intro">
+		<h1 class="header h1">Twitch Hype Bot</h1>
+	</section>
+</section>
+
+<style lang="scss">
+</style>
